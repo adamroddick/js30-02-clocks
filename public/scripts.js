086075d7329fb6f7daf59dca0f373bb818cef4bf
Adam Roddick <adam@Ads-2.local>
2019-01-28 01:16:29 +1100

    Complete working svg clock hands

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,36 @@
-var stickRotation = 30
+var secondHand = 0
+var minuteHand = 0
+var hourHand = 0
+var elem = document.querySelector('#second')
+
+function rotateSecond(increment) {
+    let elem = document.querySelector('#second')
+    secondHand = increment
+    elem.setAttribute('transform',`rotate(${secondHand} 100 100)`)
+}
+
+function rotateMinute(increment) {
+    let elem = document.querySelector('#minute')
+    minuteHand = increment
+    elem.setAttribute('transform',`rotate(${minuteHand} 100 100)`)
+}
+
+function rotateHour(increment) {
+    let elem = document.querySelector('#hour')
+    hourHand = increment
+    elem.setAttribute('transform',`rotate(${hourHand} 100 100)`)
+}
 
 function rotateStick() {
-    let div = document.querySelector('#stick')
-    div.style.transform = `rotate(${stickRotation}deg)`
-    stickRotation += 30
+    let elem = document.querySelector('#second')
+    console.log(elem)
+    second = new Date().getSeconds() * 6
+    console.log(second)
+    elem.style.transform = `rotate(${second}deg, 100, 100)`
 }
+
+window.setInterval(function() {
+    rotateSecond((new Date().getSeconds()) * 6) 
+    rotateMinute((new Date().getMinutes()) * 6)
+    rotateHour((new Date().getHours()) * 30)
+}, 1000)
